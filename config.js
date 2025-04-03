12b304495ee5d041ec800ad9439c8a9949450a16
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-04-03 10:56:19 -0400

    Adding Custom Font

--- config.js
+++ config.js
@@ -5,6 +5,10 @@ tailwind.config = {
                 pale_orange: '#ffd9a6',
                 light_orange: '#fbb03b',
                 orange: '#f7931e'
+            },
+            fontFamily: {
+                sans: ["Josefin Sans", "sans-serif"],
+                title: ["Playfair Display SC", "serif"]
             }
         }
     }
